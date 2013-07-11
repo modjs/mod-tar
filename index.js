c3f49f605171573297142391366cb59251232203
元彦 <yuanyan.cao@gmail.com>
2013-07-11 20:55:24 +0800

    init plugin

---
+++ index.js
@@ -0,0 +1,22 @@
+var tar = require('./lib/tar');
+var path = require('path');
+
+
+exports.summary = 'Create a tarball';
+
+exports.usage = '<src> [options]';
+
+exports.options = {
+    "dest" : {
+        alias : 'd'
+        ,describe : 'target pack file path'
+    }
+};
+
+exports.run = function (options, done) {
+    var source = options.src;
+    // TODO if not exist package.json
+    var cfg = exports.file.readPackageJSON(source);
+    var target = options.dest || cfg.name + '-' + cfg.version + '.tar.gz';
+    tar.create(cfg, source, target, exports, done);
+};
