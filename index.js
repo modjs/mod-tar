53f6b719fc89fbe72517416a307a549bfbd63452
yuanyan <yuanyan.cao@gmail.com>
2013-09-16 03:25:26 +0800

    Version bump

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var tar = require('./lib/tar');
 var path = require('path');
 
-
-exports.summary = 'Create a tarball';
+exports.summary = 'Create/extract a tarball';
 
 exports.usage = '<src> [options]';
 
@@ -10,13 +9,29 @@ exports.options = {
     "dest" : {
         alias : 'd'
         ,describe : 'target pack file path'
+    },
+    "action": {
+        alias : 'a'
+        ,default: 'create'
+        ,describe : 'create or extract'
     }
 };
 
 exports.run = function (options, done) {
     var source = options.src;
-    // TODO if not exist package.json
-    var cfg = exports.file.readPackageJSON(source);
-    var target = options.dest || cfg.name + '-' + cfg.version + '.tar.gz';
-    tar.create(cfg, source, target, exports, done);
+    var target = options.dest;
+    var action = options.action;
+    exports[action](source, target, done);
+};
+
+exports.create = function(source, target, done){
+    if(!target && exports.file.findPackageJSON(source)){
+        var cfg = exports.file.readPackageJSON(source);
+        target = cfg.name + '-' + cfg.version + '.tar.gz';
+    }
+    tar.create(source, target, cfg, exports, done);    
+};
+
+exports.extract = function(source, target, done) {
+    tar.extract(source, target, exports, done);
 };
